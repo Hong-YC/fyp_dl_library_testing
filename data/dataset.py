d1db0bd8e648ecbf9a51e6104c66199570029e3d
SU Hong <hsuag@sccpu1.cse.ust.hk>
2023-01-07 22:57:24 +0800

    create dataset py

---
+++ data/dataset.py
@@ -0,0 +1,37 @@
+import torch
+import torchvision
+from torch.utils.data import dataset
+import numpy as np
+
+xy = np.load("../src/data/dataset/inputs.npz")
+haha = xy['test_input']
+print(np.shape(haha))
+
+
+# a dataset class template
+class TrainDataset(Dataset):
+
+    def __init__(self, transform = None):
+        # path =  xxx
+        xy = np.load(path) 
+
+        self.n_samples
+        self.x
+        self.y
+        self.transform = transform
+
+    def __getitem__(self, index):
+        sample = self.x[index], self.y[index]
+        if self.transform:
+            sample = self.transform(sample)
+        return sample
+
+    def __len__(self):
+        return self.n_samples
+
+    class ToTensor:
+        def __call__(self, sample):
+            inputs, targets = sample
+            return torch.from_numpy(inputs), torch.from_numpy(targets)
+
+    # dataset  = Traindataset(transform = ToTensor())
