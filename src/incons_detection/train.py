c3102c2d395dcc9db2d4c6080577d49a8e8e8750
SU Hong <hsuag@sccpu1.cse.ust.hk>
2023-01-08 17:37:40 +0800

    add train.py

---
+++ src/incons_detection/train.py
@@ -0,0 +1,19 @@
+train_dataloader = DataLoader(training_data, batch_size=64)
+
+
+# training loop for PyTorch model
+def train_loop(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    for batch, (X, y) in enumerate(dataloader):
+        # Compute prediction and loss
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
