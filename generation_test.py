60222213397eac85873458dd2e7072509e68f20a
CHANG Hong-yuan <hchangac@sccpu1.cse.ust.hk>
2023-01-15 22:20:30 +0800

    Add generation_test.py for generate pytorch model

---
+++ generation_test.py
@@ -0,0 +1,14 @@
+from src.cases_generation.model_generator import ModelGenerator
+
+
+
+if __name__ == '__main__':
+
+    generator = ModelGenerator(10)
+    exp_path = "/data/fyp23-dlltest/Hong/fyp_dl_library_testing"
+    model_info_json_path = "/data/fyp23-dlltest/Hong/fyp_dl_library_testing/dummy_model.json"
+    generator.generate(model_info_json_path, exp_path)
+
+
+
+
