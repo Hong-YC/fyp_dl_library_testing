12270fbf51b3cdac3c8d107773b16424157f6275
CHANG Hong-yuan <hchangac@sccpu1.cse.ust.hk>
2023-01-26 15:59:32 +0800

    Add clear_data.py to clear the database

--- utils/selection.py
+++ utils/selection.py
@@ -21,3 +21,39 @@ class Roulette(object):
         self.__pool = {name: self.Element(name=name) for name in layer_types}
         self.__layer_conditions = layer_conditions
         self.__use_heuristic = use_heuristic
+    
+    def update(self, name):
+        if name == 'input_object':
+            return
+
+        if name in self.__pool:
+            self.__pool[name].record()
+            return 
+    
+    def coverage(self):
+        selected_map = {name: el.selected for name, el in self.__pool.items()}
+        cnt = 0
+        for selected in selected_map.values():
+            if selected > 0:
+                cnt += 1
+        coverage_rate = cnt / len(selected_map)
+        return coverage_rate, selected_map
+    
+    def choose_element(self, pool: List[str], **kwargs):
+        candidates = []
+        _sum = 0
+        for el_name in pool:
+            cond = self.__layer_conditions.get(el_name, None)
+            if cond is None or cond(**kwargs):  # available的layer
+                candidates.append(self.__pool[el_name])
+                _sum += self.__pool[el_name].score
+
+        if self.__use_heuristic:
+            rand_num = np.random.rand() * _sum
+            for el in candidates:
+                if rand_num < el.score:
+                    return el.name
+                else:
+                    rand_num -= el.score
+        else:
+            return random.choice(candidates).name
