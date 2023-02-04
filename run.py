8ddcf04996f532a3c9d3e786e026511b4b22f068
CHANG Hong-yuan <hchangac@sccpu1.cse.ust.hk>
2023-02-04 16:30:07 +0800

    implement run.py, modify modell_generator

---
+++ run.py
@@ -0,0 +1,86 @@
+from pathlib import Path
+from typing import List, Optional
+import datetime
+
+from utils.utils import get_HH_mm_ss
+from utils.db_manager import DbManager
+from utils.selection import Roulette
+from src.cases_generation.model_info_generator import ModelInfoGenerator
+from src.cases_generation.model_generator import ModelGenerator
+from src.cases_generation.data_generator import DataGenerator
+
+import os
+os.environ['MKL_THREADING_LAYER'] = 'GNU'
+
+class TrainingDebugger(object):
+    def __init__(self, config: dict, use_heuristic: bool = True, generate_mode: str = 'template', timeout: float = 60):
+        super().__init__()
+        self.__output_dir = config['output_dir']
+        self.__db_manager = DbManager(config['db_path'])
+        # Roulette selector
+        from utils.utils import layer_types, layer_conditions
+        self.__selector = Roulette(layer_types=layer_types,
+                                   layer_conditions=layer_conditions,
+                                   use_heuristic=use_heuristic)
+        self.__model_info_generator = ModelInfoGenerator(config['model'], generate_mode, self.__db_manager, self.__selector)
+        self.__model_generator = ModelGenerator(self.__db_manager, self.__selector, timeout)
+        self.__training_data_generator = DataGenerator(config['training_data'])
+        # self.__weights_trainer = Trainer(self.__db_manager, timeout)
+        # self.__weights_comparator = Comparator(self.__db_manager)
+
+
+
+def main(testing_config):
+    config = {
+        'model': {
+            'var': {
+                'tensor_dimension_range': (2, 5),
+                'tensor_element_size_range': (2, 5),
+                'weight_value_range': (-10.0, 10.0),
+                'small_value_range': (0, 1),
+                'vocabulary_size': 1001,
+            },
+            'node_num_range': (5, 5),
+            'dag_io_num_range': (1, 3),
+            'dag_max_branch_num': 2,
+            'cell_num': 3,
+            'node_num_per_normal_cell': 10,
+            'node_num_per_reduction_cell': 2,
+        },
+        'training_data': {
+            'instance_num': 10,
+            'element_val_range': (0, 100),
+        },
+        'db_path': str(Path.cwd() / testing_config['data_dir'] / f'{testing_config["dataset_name"]}.db'),
+        'output_dir': str(Path.cwd() / testing_config['data_dir'] / f'{testing_config["dataset_name"]}_output'),
+        'report_dir': str(Path.cwd() / testing_config['data_dir'] / f'{testing_config["dataset_name"]}_report'),
+        'distance_threshold': 0,
+    }
+
+    CASE_NUM = testing_config["case_num"]
+    TIMEOUT = testing_config["timeout"]  # Second
+    USE_HEURISTIC = bool(testing_config["use_heuristic"])  
+    GENERATE_MODE = testing_config["generate_mode"]  # seq\merging\dag\template
+
+    debugger = TrainingDebugger(config, USE_HEURISTIC, GENERATE_MODE, TIMEOUT)
+    start_time = datetime.datetime.now()
+
+
+
+    for i in range(CASE_NUM):
+            print(f"######## Round {i} ########")
+            try:
+                print("------------- generation -------------")
+                model_id, exp_dir, ok_backends = debugger.run_generation()
+                # print("------------- detection -------------")
+                # ok_backends = debugger.run_detection(model_id, exp_dir, ok_backends)
+            except Exception:
+                import traceback
+                traceback.print_exc()
+        
+
+    end_time = datetime.datetime.now()
+    time_delta = end_time - start_time
+    h, m, s = get_HH_mm_ss(time_delta)
+    print(f"Finish execution: Time used: {h} hour,{m} min,{s} sec")
+    
--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,6 @@
+import os
 
+use_gpu = True
 
 def construct_layer_name(layer_id, layer_type, cell_type=''):
     return str(layer_id).zfill(2) + '_' + layer_type + (('' if cell_type == '' else '_') + cell_type)
@@ -11,16 +13,24 @@ def get_layer_func(layer_type):
     else:
         return getattr(torch.nn, layer_type)
 
+def get_HH_mm_ss(td):
+    days, seconds = td.days, td.seconds
+    hours = days * 24 + seconds // 3600
+    minutes = (seconds % 3600) // 60
+    secs = seconds % 60
+    return hours, minutes, secs
+
+
 
 torch_layer = [
     'reshape'
 ]
 
-torch_nn_layer = [
-    'Linear',
-    'Conv2d',
-    'Flatten'
-]
+# torch_nn_layer = [
+#     'Linear',
+#     'Conv2d',
+#     'Flatten'
+# ]
 seq_layer_types = [
     'Linear',
     'Conv1d',
@@ -40,7 +50,26 @@ activation_layer_types = [
 ]
 
 # layer_types = seq_layer_types
-layer_types = activation_layer_types
+layer_types = seq_layer_types + activation_layer_types + torch_layer
+
+
+# TODO: Improve rules
+activation_cond = (lambda **kwargs: kwargs['e1'] in [*seq_layer_types, *torch_layer])
+
+dim_3_cond = (lambda **kwargs: kwargs.get('input_dim', None) == 3)
+dim_4_cond = (lambda **kwargs: kwargs.get('input_dim', None) == 4)
+dim_5_cond = (lambda **kwargs: kwargs.get('input_dim', None) == 5)
+
+# Rules for connecting layers
+layer_conditions = {
+    **{layer_name: activation_cond for layer_name in activation_layer_types},
+    'Conv1d': dim_3_cond,
+    'Conv2d': dim_4_cond,
+    'Conv3d': dim_5_cond,
+    # Since we skip the batch dim when flattening
+    'Flatten': (lambda **kwargs: kwargs.get('input_dim', None) >= 3)
+}
+
 
 if __name__ == '__main__':
     c = get_layer_func("Conv2d")
