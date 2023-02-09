eacdc1e80dfc6a74acc580f8b9edc46185de74cd
DGENJI <jzhaobh@connect.ust.hk>
2023-02-09 14:15:23 +0800

    a

--- comparator.py
+++ comparator.py
@@ -8,6 +8,7 @@ import onnxruntime
 import numpy as np
 import collections
 from utils.tool import *
+from utils.db_manager import *
 from src.cases_generation.torch_model import TorchModel
 
 pair={}
@@ -19,60 +20,83 @@ pair['Flatten']='Flatten'
 pair['reshape']='Reshape'
 pair['LeakyReLU']='LeakyRelu'
 
-class Comparator(object):
+class comparator(object):
 
-    def __init__(self, dir_torch, dir_json, dir_onnx):
+    def __init__(self, dir_torch, dir_json, dir_onnx, db_m, model_id):
         super().__init__()
-        self.dir_torch=dir_torch
-        self.dir_json=dir_json
-        self.dir_onnx=dir_onnx
-
+        self.__dir_torch=dir_torch
+        self.__dir_json=dir_json
+        self.__dir_onnx=dir_onnx
+        self.__db_m=db_m
+        self.__model_id=model_id
+        
         #prepare models
-        with open(self.dir_json, 'r') as f:
-            self.model_info = json.load(f)
-        self.model_torch=TorchModel(self.model_info)
-        self.model_torch.load_state_dict(torch.load(self.dir_torch))
-
+        with open(self.__dir_json, 'r') as f:
+            self.__model_info = json.load(f)
+        self.__model_torch=TorchModel(self.__model_info)
+        self.__model_torch.load_state_dict(torch.load(self.__dir_torch))
+        
         #load onnx model
-        self.model_onnx = onnx.load(self.dir_onnx)
-        
-        
+        self.__model_onnx = onnx.load(self.__dir_onnx)
         
     def extract(self, input):
         #extract outputs
-        self.output_torch=extract_inter_output_pytorch(self.model_torch, input)
-        self.name_list=list(self.output_torch.keys())
+        self.__output_torch=extract_inter_output_pytorch(self.__model_torch,input)
+        self.__name_list=list(self.__output_torch.keys())
             
-        outs=extract_inter_output_onnx(self.model_onnx,input.numpy())
-        self.output_onnx={}
-        self.output_onnx['out']=outs['out']
+        outs=extract_inter_output_onnx(self.__model_onnx,input.numpy())
+        self.__output_onnx={}
+        self.__output_onnx['out']=outs['out']
         cou=0
-        for node in self.model_onnx.graph.node:
-            if cou==len(self.name_list):
+        for node in self.__model_onnx.graph.node:
+            if cou==len(self.__name_list):
                 break
-            if pair[self.model_info['model_structure'][self.name_list[cou][-1]]['type']] in node.name:
-                self.output_onnx[self.name_list[cou]]=outs[node.output[0]]
+            if pair[self.__model_info['model_structure'][self.__name_list[cou][-1]]['type']] in node.name:
+                self.__output_onnx[self.__name_list[cou]]=outs[node.output[0]]
                 cou=cou+1
 
     def compare_inference(self, input, epsilon=1e-4):
         O_torch=None
         O_onnx=None
+        crash=False
         try:
-            O_torch=list(self.model_torch(input).values())[0].detach().numpy()
-        except:
-            return None
+            O_torch=list(self.__model_torch(input).values())[0].detach().numpy()
+        except Exception:
+            crash=True
+            self.__db_m.update_model_crash_backends(self.__model_id, ['torch'])
         try:
-            sess = onnxruntime.InferenceSession(self.dir_onnx)
+            sess = onnxruntime.InferenceSession(self.__dir_onnx)
             input_name = sess.get_inputs()[0].name
             label_name = sess.get_outputs()[0].name
             ins=input.numpy()
             result = sess.run(None, {input_name: ins})
             O_onnx=result[0]
-        except:
+        except Exception:
+            crash=True
+            self.__db_m.update_model_crash_backends(self.__model_id, ['onnx'])
+        if crash:
             return None
+        
+        comparable=True
+        if (True in np.isinf(O_torch)):
+            update_model_inf_backends(self.__model_id, ['torch'])
+            comparable=False
+        if (True in np.isnan(O_torch)):
+            update_model_nan_backends(self.__model_id, ['torch'])
+            comparable=False
+        if (True in np.isinf(O_onnx)):
+            update_model_inf_backends(self.__model_id, ['onnx'])
+            comparable=Falsev
+        if (True in np.isnan(O_onnx)):
+            update_model_nan_backends(self.__model_id, ['onnx'])
+            comparable=False
+        
+        if not comparable:
+            return None
+        
         dif=norm_delta(O_torch, O_onnx)
         if dif>epsilon:
-            return dif
+            add_inconsistencies([(self.__model_id,input,dif)])
             
     def norm_delta(self, x, y, mode="max"):
         # infinite norm
@@ -88,17 +112,17 @@ class Comparator(object):
             return None
 
     def model_output_delta(self):
-        if self.output_onnx is None or self.output_torch is None:
+        if self.__output_onnx is None or self.__output_torch is None:
             return None
         output_deltas = {}
-        for layer_name in self.name_list:
-            o1=self.output_onnx[layer_name]
-            o2=self.output_torch[layer_name]
+        for layer_name in self.__name_list:
+            o1=self.__output_onnx[layer_name]
+            o2=self.__output_torch[layer_name]
             output_deltas.append(layer_name,norm_delta(o1, o2))
         return output_deltas
 
     def layer_output_ratio(self, epsilon=1e-7):
-        if self.output_onnx is None or self.output_torch is None:
+        if self.__output_onnx is None or self.__output_torch is None:
             return None
         output_deltas=model_output_delta()
         output_ratio={}
--- converter.py
+++ converter.py
@@ -7,30 +7,25 @@ from src.cases_generation.torch_model import TorchModel
 import json
 import collections
 
-class Converter(object):
+class converter(object):
 
     def __init__(self, dir_torch, dir_json, dir_onnx):
         super().__init__()
-        self.dir_torch=dir_torch
-        self.dir_json=dir_json
-        self.dir_onnx=dir_onnx
+        self.__dir_torch=dir_torch
+        self.__dir_json=dir_json
+        self.__dir_onnx=dir_onnx
         
     def convert(self, version=12):
-        with open(self.dir_json, 'r') as f:
+        with open(self.__dir_json, 'r') as f:
             model_info = json.load(f)
-        model_torch = TorchModel(model_info)
-        model_torch.load_state_dict(torch.load(self.dir_torch))
-        input_shape = model_info['model_structure']['0']['args']['shape']
-        # Batch size equals 1
-        input_shape.insert(0, 1)
-        dummy_input = torch.ones(input_shape)
-        try:
-            torch.onnx.export(
-                model_torch, dummy_input, self.dir_onnx,
-                export_params=True,
-                output_names=["out"],
-                opset_version=version
-            )
-        except:
-            # log into database if conversion fail
-            
+        model_torch=TorchModel(model_info)
+        model_torch.load_state_dict(torch.load(self.__dir_torch))
+        s=model_info['model_structure']['0']['args']['shape']
+        s.insert(0, 1)
+        dummy_input = torch.ones(s)
+        torch.onnx.export(
+            model_torch, dummy_input, self.__dir_onnx,
+            export_params=True,
+            output_names=["out"],
+            opset_version=version
+        )
