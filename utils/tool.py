a9b88da4d130729d9ab583d24f21edf3d978fd13
SU Hong <hsuag@sccpu1.cse.ust.hk>
2023-03-11 17:40:50 +0800

    extract tf intermediate

--- utils/tool.py
+++ utils/tool.py
@@ -4,6 +4,11 @@ import onnx
 from onnx import numpy_helper
 import torch
 import collections
+from src.cases_generation.torch_model import TorchModel
+from onnx_tf.backend import prepare
+import tensorflow as tf
+import onnx_tf
+import json
 
 
 # some helper functions to check weight
@@ -39,6 +44,13 @@ def ort_inference(onnx_path, input):
     ort_outputs = ort_session.run(None, ort_inputs)
     return ort_outputs
 
+#  input a onnx model and a numpy array, output the inference result in tensorflow
+def tf_inference(onnx_model, np_arr):
+    tf_rep = prepare(onnx_model)
+    tf_input = tf.convert_to_tensor(np_arr, dtype = tf.float32)
+    return tf_rep.run(tf_input)
+
+
 
 # extract intermediate outputs of each layers of PyTorch models
 def extract_inter_output_pytorch(model,input):
@@ -73,3 +85,48 @@ def extract_inter_output_onnx(model,input):
     outs = session.run(outputs, {input_name:input})
     outs = collections.OrderedDict(zip(outputs, outs))
     return outs
+
+# if tensorflow inference output is different from others, extract tf model's intermediate output (output format is numpy array)
+# the input model is PyTorch json file, input is a numpy array
+def extract_inter_output_tensorflow(pytorch_json_path, input):
+    output_list = []
+    device = "cpu"
+    if torch.cuda.is_available():
+        device = "cuda:0"
+    with open(pytorch_json_path, 'r') as f:
+        m_info = json.load(f)
+    dic = m_info["model_structure"]
+
+    for key in dic:
+        if key == '0':
+            continue
+        # first generate PyTorch model
+        temp_info = m_info.copy()
+        model_subset = dict(list(dic.items())[:int(key)+1])
+        temp_info['model_structure'] = model_subset
+        temp_info['output_id_list'] = list(key)
+        py_model = TorchModel(temp_info)
+        py_model = py_model.to(device)
+
+        # convert the PyTorch model into ONNX
+        input_shape = input.shape
+        dummy_input = torch.ones(*input_shape).to(device)
+        model_onnx_path = "model.onnx"
+        torch.onnx.export(
+            py_model, dummy_input, model_onnx_path,
+            export_params=True,
+            opset_version=11,  # version can be >=7 <=16
+            # We define axes as dynamic to allow batch_size > 1
+        )
+
+        # conver the ONNX model into tensorflowRep
+        onnx_model = onnx.load("model.onnx")
+        # load onnx into tf_rep model
+        tf_rep = prepare(onnx_model)
+        # tf_rep.export_graph("output/model.pb")
+        tf_input = tf.convert_to_tensor(input, dtype = tf.float32)
+        tf_output = tf_rep.run(tf_input)
+        output_list.append(np.array(tf_output))
+    return output_list
+
+        
